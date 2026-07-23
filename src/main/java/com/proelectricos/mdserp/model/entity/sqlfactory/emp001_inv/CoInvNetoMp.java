060616e98187e4c1250763f13825ae8d0a7561bc
pare1610 <pare1610@outlook.com>
2026-07-23 15:29:09 -0500

    Add CoTiempoCecoOP and TbPedidos entities with corresponding fields and annotations for inventory and order management

--- src/main/java/com/proelectricos/mdserp/model/entity/sqlfactory/emp001_inv/CoInvNetoMp.java
+++ src/main/java/com/proelectricos/mdserp/model/entity/sqlfactory/emp001_inv/CoInvNetoMp.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.Immutable;
 import org.hibernate.annotations.Nationalized;
 
@@ -53,7 +54,7 @@ public class CoInvNetoMp {
     @Column(name = "EXIST", precision = 14, scale = 4)
     private BigDecimal EXIST;
 
-    @Column(name = "CANT_OC", precision = 14, scale = 4)
+    @Column(name = "CANT_OC", precision = 38, scale = 4)
     private BigDecimal CANT_OC;
 
     @Column(name = "SMAX", precision = 14, scale = 4)
@@ -79,31 +80,26 @@ public class CoInvNetoMp {
 
     @Size(max = 30)
     @NotNull
-    @Nationalized
     @Column(name = "Negocio", nullable = false, length = 30)
     private String Negocio;
 
     @Size(max = 50)
     @NotNull
-    @Nationalized
     @Column(name = "Linea", nullable = false, length = 50)
     private String Linea;
 
     @Size(max = 80)
     @NotNull
-    @Nationalized
     @Column(name = "Clase", nullable = false, length = 80)
     private String Clase;
 
     @Size(max = 50)
     @NotNull
-    @Nationalized
     @Column(name = "Grupo", nullable = false, length = 50)
     private String Grupo;
 
     @Size(max = 60)
     @NotNull
-    @Nationalized
     @Column(name = "Nivel5", nullable = false, length = 60)
     private String Nivel5;
 
--- src/main/java/com/proelectricos/mdserp/repository/sqlfactory/emp001_inv/CoInvNetoMpRepository.java
+++ src/main/java/com/proelectricos/mdserp/repository/sqlfactory/emp001_inv/CoInvNetoMpRepository.java
@@ -7,5 +7,3 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CoInvNetoMpRepository extends JpaRepository<CoInvNetoMp, String> {
 }
-
-
